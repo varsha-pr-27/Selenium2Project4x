230d1067971137ce852fde00601452d05c693027
varsha-pr-27 <varshaprvviet@gmail.com>
2025-02-19 14:01:37 +0530

    added

--- src/ex_17102024/test_lab005.py
+++ src/ex_17102024/test_lab005.py
@@ -1,3 +1,5 @@
+# Program to open browser in incognito, maximum, minimum, headless mode
+
 # Chrome Options
 import time
 
@@ -9,11 +11,13 @@ def test_chrome_options():
     chrome_options = Options()
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--window-size = 900,600")
+    chrome_options.add_argument("--window-size=900,600")
+    chrome_options.add_argument("--headless")
 
     driver = webdriver.Chrome(chrome_options)
     driver.get("https://katalon-demo-cura.herokuapp.com/")
 
+    assert True == False
+
     time.sleep(10)
-
-
+    driver.quit()
